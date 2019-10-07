bc3419d824b89be1ed6d8295a23fabad7ecec3f3
kkin <kacper.kin28@gmail.com>
2019-10-07 20:51:31 +0200

    rewriter added; damaged sequences deleted

--- GenerateTasksHEVC_rewriter.py
+++ GenerateTasksHEVC_rewriter.py
@@ -8,8 +8,7 @@ COMPUTER_NUM = 1   # Liczba komputerow
 #===============================================================================
 MAX_FRAMES = 0
 #===============================================================================
-GEN_TRANSCODE = 0
-GEN_PSNR = 1
+GEN_TRANSCODE = 1
 #===============================================================================
 GEN_ALL_CFG   = 0
 #===============================================================================
@@ -17,15 +16,15 @@ GEN_SCRIPT_TYPES = ["tsk","bat","sh"] # skrypty taskow do wygenerowania
 #GEN_SCRIPT_TYPES = ["tsk"] # skrypty taskow do wygenerowania
 #===============================================================================
 s2do = [
-#    "Traffic"            ,
-#    "PeopleOnStreet"     ,
-#    "Nebuta"             ,
-#    "SteamLocomotive"    ,
-#	"Kimono1"            ,
-#    "ParkScene"          ,
-#    "Cactus"             ,
-#    "BQTerrace"          ,
-#    "BasketballDrive"    ,
+    "Traffic"            ,
+    "PeopleOnStreet"     ,
+    "Nebuta"             ,
+    "SteamLocomotive"    ,
+    "Kimono1"            ,
+    "ParkScene"          ,
+    "Cactus"             ,
+    "BQTerrace"          ,
+    "BasketballDrive"    ,
     "RaceHorses"         ,
     "BQMall"             ,
     "PartyScene"         ,
@@ -34,9 +33,9 @@ s2do = [
     "BQSquare"           ,
     "BlowingBubbles"     ,
     "BasketballPass"     ,
-#    "FourPeople"         ,
-#    "Johnny"             ,
-#    "KristenAndSara"     ,
+    "FourPeople"         ,
+    "Johnny"             ,
+    "KristenAndSara"     ,
     "BasketballDrillText",
     "ChinaSpeed"         ,
     "SlideEditing"       ,
@@ -78,14 +77,12 @@ REC_PATH = "rec"
 
 CFG_PATH_TRANS = "cfg_trans"
 LOG_PATH_TRANS = "log_trans"
-
 TASK_PATH_TRANS = "tasks%dof%d_hevc_trans"
 
-CFG_TEMPLATE_FILENAME_TRANS = BASE_CONFIG_PATH+"/config_transcoder.cfg"
-# CFG_TEMPLATE_FILENAME_TRANS = BASE_CONFIG_PATH+"/config_rewriter.cfg"
+# CFG_TEMPLATE_FILENAME_TRANS = BASE_CONFIG_PATH+"/config_transcoder.cfg"
+CFG_TEMPLATE_FILENAME_TRANS = BASE_CONFIG_PATH+"/config_rewriter.cfg"
 
 BIN_PATH_IN = "../bin"
-YUV_PATH = "../seq_HEVC"
 
 #===============================================================================
 
@@ -190,79 +187,45 @@ def create_bitstream_filename(s,qp):
     BITSTREAM_FILENAME_TEMPLATE = "../../%s/%s_%dx%d_%d_%dbit_bin_QP%01d.bin"
     return BITSTREAM_FILENAME_TEMPLATE%(BIN_PATH_IN,s2name[s],s2resolution[s][0],s2resolution[s][1],s2framerate[s],s2bitdepth[s],qp)
 
-def create_yuv_filename(s):
-    YUV_FILENAME_TEMPLATE = "../../%s/%s_%dx%d_%d_%dbit.yuv"
-    return YUV_FILENAME_TEMPLATE%(YUV_PATH,s2name[s],s2resolution[s][0],s2resolution[s][1],s2framerate[s],s2bitdepth[s])
-
-def create_output_bitstream_filename(s,qp):
-    OUTPUT_BITSTREAM_FILENAME_TEMPLATE = "../../%s/%s_%dx%d_%d_%dbit_bin_QP%02d_out.bin"
-    return OUTPUT_BITSTREAM_FILENAME_TEMPLATE%(BIT_PATH,s2name[s],s2resolution[s][0],s2resolution[s][1],s2framerate[s],s2bitdepth[s],qp)
+def create_output_bitstream_filename(s,qp,mode):
+    OUTPUT_BITSTREAM_FILENAME_TEMPLATE = "../../%s/%s_%dx%d_%d_%dbit_bin_QP%02d__mode%01d_out.bin"
+    return OUTPUT_BITSTREAM_FILENAME_TEMPLATE%(BIT_PATH,s2name[s],s2resolution[s][0],s2resolution[s][1],s2framerate[s],s2bitdepth[s],qp,mode)
 
 #===========================TRANSCODER==========================================
-def create_task_id_name_trans(s,qp): 
+def create_task_id_name_trans(s,qp,mode): 
     #Create task id name form sequence number and qp value
-    TASK_ID_NAME_TEMPLATE = "hevc_transcoder_%s_%dx%d_%d_%dbit_QP%02d"
-    return TASK_ID_NAME_TEMPLATE%(s2name[s],s2resolution[s][0],s2resolution[s][1],s2framerate[s],s2bitdepth[s],qp)
+    TASK_ID_NAME_TEMPLATE = "hevc_transcoder_%s_%dx%d_%d_%dbit_QP%02d_mode%01d"
+    return TASK_ID_NAME_TEMPLATE%(s2name[s],s2resolution[s][0],s2resolution[s][1],s2framerate[s],s2bitdepth[s],qp,mode)
 
 def create_task_filename_trans(task_idcn,task_id_name):
     #Create task id name form sequence number and qp value
     TASK_FILENAME_TEMPLATE = "../%s/%s"
     return (TASK_FILENAME_TEMPLATE%(TASK_PATH_TRANS,task_id_name))%(task_idcn,len(COMPUTER_NUM))
 
-def create_task_id_name_psnr(s,qp): 
+def create_cfg_filename_trans(s,qp,mode):
     #Create task id name form sequence number and qp value
-    TASK_ID_NAME_TEMPLATE = "hevc_psnr_%s_%dx%d_%d_%dbit_QP%02d"
-    return TASK_ID_NAME_TEMPLATE%(s2name[s],s2resolution[s][0],s2resolution[s][1],s2framerate[s],s2bitdepth[s],qp)
+    CFG_FILENAME_TEMPLATE = "../../%s/%s_QP%02d_mode%01d.cfg"
+    return CFG_FILENAME_TEMPLATE%(CFG_PATH_TRANS,s2name[s],qp,mode)
 
-def create_task_filename_psnr(task_idcn,task_id_name):
+def create_log_filename_trans(s,qp,mode):
     #Create task id name form sequence number and qp value
-    TASK_FILENAME_TEMPLATE = "../%s/%s"
-    return (TASK_FILENAME_TEMPLATE%(TASK_PATH_TRANS,task_id_name))%(task_idcn,len(COMPUTER_NUM))
+    LOG_FILENAME_TEMPLATE = "../../%s/%s_QP%02d_mode%01d_log.txt"
+    return LOG_FILENAME_TEMPLATE%(LOG_PATH_TRANS,s2name[s],qp,mode)
 
-def create_cfg_filename_trans(s,qp):
+def create_err_filename_trans(s,qp,mode):
     #Create task id name form sequence number and qp value
-    CFG_FILENAME_TEMPLATE = "../../%s/%s_QP%02d.cfg"
-    return CFG_FILENAME_TEMPLATE%(CFG_PATH_TRANS,s2name[s],qp)
-
-def create_log_filename_trans(s,qp):
-    #Create task id name form sequence number and qp value
-    LOG_FILENAME_TEMPLATE = "../../%s/%s_QP%02d_log.txt"
-    return LOG_FILENAME_TEMPLATE%(LOG_PATH_TRANS,s2name[s],qp)
-
-def create_err_filename_trans(s,qp):
-    #Create task id name form sequence number and qp value
-    LOG_FILENAME_TEMPLATE = "../../%s/%s_QP%02d_err.txt"
-    return LOG_FILENAME_TEMPLATE%(LOG_PATH_TRANS,s2name[s],qp)
-
-def create_log_filename_psnr_org(s,qp):
-    #Create task id name form sequence number and qp value
-    LOG_FILENAME_TEMPLATE = "../../%s/%s_QP%02d_log_psnr_org.txt"
-    return LOG_FILENAME_TEMPLATE%(LOG_PATH_TRANS,s2name[s],qp)
-
-def create_err_filename_psnr_org(s,qp):
-    #Create task id name form sequence number and qp value
-    LOG_FILENAME_TEMPLATE = "../../%s/%s_QP%02d_err_psnr_org.txt"
-    return LOG_FILENAME_TEMPLATE%(LOG_PATH_TRANS,s2name[s],qp)
+    LOG_FILENAME_TEMPLATE = "../../%s/%s_QP%02d_mode%01d_err.txt"
+    return LOG_FILENAME_TEMPLATE%(LOG_PATH_TRANS,s2name[s],qp,mode)
 
 def create_commandline_trans():
     #Create task id name form sequence number and qp value
     COMMAND_LINE_TEMPLATE = "../../%s/HEVC.out"
     return COMMAND_LINE_TEMPLATE%(BIN_PATH)
 
-def create_argline_trans(s,qp):
+def create_argline_trans(s,qp,mode):
     #Create task id name form sequence number and qp value
-    ARG_LINE_TEMPLATE = "-T -c %s"
-    return ARG_LINE_TEMPLATE%(create_cfg_filename_trans(s, qp))
-
-def create_commandline_psnr():
-    #Create task id name form sequence number and qp value
-    COMMAND_LINE_TEMPLATE = "../../%s/psnr.out"
-    return COMMAND_LINE_TEMPLATE%(BIN_PATH)
-
-def create_argline_psnr_org(s,qp):
-    #Create task id name form sequence number and qp value
-    ARGLINE_TEMPLATE = "-i1 %s -i2 %s -dx %d -dy %d"
-    return ARGLINE_TEMPLATE%(create_yuv_filename(s),create_bitstream_filename(s,qp),s2resolution[s][0],s2resolution[s][1])
+    ARG_LINE_TEMPLATE = "-R -c %s"
+    return ARG_LINE_TEMPLATE%(create_cfg_filename_trans(s, qp, mode))
 
 #===============================================================================
 
@@ -274,7 +237,7 @@ def prepare_paths_or_del(remove_or_create):
     #global LOG_PATH_TRANS
     #global RUN_PATH
 
-    if (GEN_TRANSCODE|GEN_PSNR|GEN_ALL_CFG|remove_or_create):
+    if (GEN_TRANSCODE|GEN_ALL_CFG|remove_or_create):
 
         ensure_path_or_del(remove_or_create, PATH+"/"+BIT_PATH)
         ensure_path_or_del(remove_or_create, PATH+"/"+CFG_PATH_TRANS)
@@ -312,15 +275,32 @@ def modify_parameter(config, new_parameter_name, new_parameter_value):
 
 #===============================================================================
 
-def prepare_config_trans(s,qp):
+def prepare_config_trans(s,qp,mode):
     ref_cfg_file = open("../../"+CFG_TEMPLATE_FILENAME_TRANS,'r')
     config = ref_cfg_file.readlines()
     ref_cfg_file.close()
 
     config = modify_parameter(config, "InputBitstreamFile", create_bitstream_filename(s,qp))
-    config = modify_parameter(config, "OutputBitstreamFile", create_output_bitstream_filename(s,qp))
+    config = modify_parameter(config, "OutputBitstreamFile", create_output_bitstream_filename(s,qp,mode))
+
+    config = modify_parameter(config, "RewritingMode", mode)
+    #config = modify_parameter(config, "InputBitDepth", str(s2bitdepth[s]))
+    #config = modify_parameter(config, "OutputBitDepth", str(s2bitdepth[s]))
+    #
+    #config = modify_parameter(config, "SourceWidth", str(s2resolution[s][0]))
+    #config = modify_parameter(config, "SourceHeight", str(s2resolution[s][1]))
+    #
+    #config = modify_parameter(config, "FrameRate", str(s2framerate[s]))
+    #
+    #if (MAX_FRAMES<=0):
+    #    frames = s2frames[s]
+    #else:
+    #    frames = min(MAX_FRAMES, s2frames[s])
+    #config = modify_parameter(config, "FramesToBeEncoded", str(frames))
+    #
+    #config = modify_parameter(config, "QP", str(qp))
     
-    cfg_filename = create_cfg_filename_trans(s, qp)
+    cfg_filename = create_cfg_filename_trans(s, qp, mode)
     cfg = open(cfg_filename,'w')
     cfg.writelines(config)
     cfg.close()
@@ -372,63 +352,46 @@ for i in range(len(COMPUTER_NUM)):
     tests.append(0)
 
 #test_idx=0
-for s in s2do: #Loop over every sequence to do
-    for qp in range(10,52): #Loop over qp range for given seqence
+for mode in range(0,5):	# Loop over all operation modes
+    for s in s2do: #Loop over every sequence to do
+        for qp in range(10,52): #Loop over qp range for given seqence
 
-        best_i = -1
-        best_dif = 0
-        for j in range(len(COMPUTER_NUM)):
-            i = computers[j]
-            if (COMPUTER_NUM[i]>0):
-                dif = COMPUTER_NUM[i] - tests[i]/float(max(tests_done,1))
-                if ((dif>best_dif) | (best_i<0)):
-                    best_dif = dif
-                    best_i   = i
+            best_i = -1
+            best_dif = 0
+            for j in range(len(COMPUTER_NUM)):
+                i = computers[j]
+                if (COMPUTER_NUM[i]>0):
+                    dif = COMPUTER_NUM[i] - tests[i]/float(max(tests_done,1))
+                    if ((dif>best_dif) | (best_i<0)):
+                        best_dif = dif
+                        best_i   = i
 
-        tests_done += 1
+            tests_done += 1
 
-        tests[best_i] += 1
-        task_idcn = best_i+1
+            tests[best_i] += 1
+            task_idcn = best_i+1
 
-        print("  HTM GEN("+str(task_idcn)+"/"+str(len(COMPUTER_NUM)) + ")   "+s2name[s]+" QP:"+str(qp) )
-        last_task_filename = ""
+            print("  HTM GEN("+str(task_idcn)+"/"+str(len(COMPUTER_NUM)) + ")   "+s2name[s]+" QP:"+str(qp) )
+            last_task_filename = ""
 
-#        transcoding
-        task_id_name_trans = create_task_id_name_trans(s,qp)
-        if (GEN_TRANSCODE|GEN_ALL_CFG):
-            ensure_path(PATH+"/"+RUN_PATH+"/"+task_id_name_trans)
-            os.chdir(PATH+"/"+RUN_PATH+"/"+task_id_name_trans)
+#           transcoding
+            task_id_name_trans = create_task_id_name_trans(s,qp,mode)
+            if (GEN_TRANSCODE|GEN_ALL_CFG):
+                ensure_path(PATH+"/"+RUN_PATH+"/"+task_id_name_trans)
+                os.chdir(PATH+"/"+RUN_PATH+"/"+task_id_name_trans)
 
-            prepare_config_trans(s,qp)
+                prepare_config_trans(s,qp,mode)
 
-#        psnr
-        task_id_name_psnr = create_task_id_name_psnr(s,qp)
-        if (GEN_PSNR):
-            ensure_path(PATH+"/"+RUN_PATH+"/"+task_id_name_psnr)
-            os.chdir(PATH+"/"+RUN_PATH+"/"+task_id_name_psnr)
+            log_filename = create_log_filename_trans(s,qp,mode)
+            err_filename = create_err_filename_trans(s,qp,mode)
+            commandline  = create_commandline_trans()
+            argline      = create_argline_trans(s,qp,mode)
 
-        log_filename_trans = create_log_filename_trans(s,qp)
-        err_filename_trans = create_err_filename_trans(s,qp)
-
-        log_filename_psnr_org   = create_log_filename_psnr_org(s,qp)
-        err_filename_psnr_org   = create_err_filename_psnr_org(s,qp)
-
-        commandline        = create_commandline_trans()
-        argline            = create_argline_trans(s,qp)
-        commandline_psnr   = create_commandline_psnr()
-        argline_psnr_org   = create_argline_psnr_org(s,qp)
-
-        os.chdir(PATH+"/"+RUN_PATH+"/")
-        if (GEN_PSNR):
-            new_task_filename = create_task_filename_psnr(task_idcn,task_id_name_psnr)
-            generate_task_v2(GEN_SCRIPT_TYPES, new_task_filename, RUN_PATH+"/"+task_id_name_psnr, [commandline_psnr,argline_psnr_org], [log_filename_psnr_org,err_filename_psnr_org], MB_thread_usage_trans[s2class[s]], USER, sys.argv[0], EMAIL_ENABLE, EMAIL_RECIPIENTS, FINAL_COMMENTS_TRANS, last_task_filename)
-            last_task_filename = new_task_filename
-
-        os.chdir(PATH+"/"+RUN_PATH+"/")
-        if (GEN_TRANSCODE):
-            new_task_filename = create_task_filename_trans(task_idcn,task_id_name_trans)
-            generate_task_v2(GEN_SCRIPT_TYPES, new_task_filename, RUN_PATH+"/"+task_id_name_trans, [commandline,argline], [log_filename_trans,err_filename_trans], MB_thread_usage_trans[s2class[s]], USER, sys.argv[0], EMAIL_ENABLE, EMAIL_RECIPIENTS, FINAL_COMMENTS_TRANS, last_task_filename)
-            last_task_filename = new_task_filename
+            os.chdir(PATH+"/"+RUN_PATH+"/")
+            if (GEN_TRANSCODE):
+                new_task_filename = create_task_filename_trans(task_idcn,task_id_name_trans)
+                generate_task_v2(GEN_SCRIPT_TYPES, new_task_filename, RUN_PATH+"/"+task_id_name_trans, [commandline,argline], [log_filename,err_filename], MB_thread_usage_trans[s2class[s]], USER, sys.argv[0], EMAIL_ENABLE, EMAIL_RECIPIENTS, FINAL_COMMENTS_TRANS, last_task_filename)
+                last_task_filename = new_task_filename
 
 for i in range(len(COMPUTER_NUM)):
     print("TESTS FOR ", (i+1), ":  ", tests[i]);
